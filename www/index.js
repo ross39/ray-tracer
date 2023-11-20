8535c0c027dcbea584bd9896fc9a7ae86a7b141e
ross_heaney <Rheaney26@gmail.com>
2023-11-20 07:49:52 +0000

    improvememts

--- www/index.js
+++ www/index.js
@@ -1,13 +1,41 @@
-import { Universe } from "wasm-game-of-life";
+import {Universe} from "wasm-game-of-life";
 
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 
 const renderLoop = () => {
-  pre.textContent = universe.render();
-  universe.tick();
+    pre.textContent = universe.render();
+    universe.tick();
 
-  requestAnimationFrame(renderLoop);
+    requestAnimationFrame(renderLoop);
 };
 
+const bitIsSet = (n, arr) => {
+    const byte = Math.floor(n / 8);
+    const mask = 1 << (n % 8);
+    return (arr[byte] & mask) === mask;
+}
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+
+            ctx.fillStyle = bitIsSet(idx, cells)
+                ? ALIVE_COLOR
+                : DEAD_COLOR;
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+}
 requestAnimationFrame(renderLoop);
